bcf21450c0a0216b1d9c3e67625d8157ce9607f0
Александр Рыжаков <rjizhakov@yandex.ru>
2025-09-11 23:23:57 +0500

    fix: little bug

--- src/main/java/ru/yandex/practicum/catsgram/controller/ImageController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/ImageController.java
@@ -20,7 +20,7 @@ public class ImageController {
         return imageService.getPostImages(postId);
     }
 
-    @GetMapping(value = "/images/{imagesId}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    @GetMapping(value = "/images/{imageId}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     public ResponseEntity<byte[]> downloadImage(@PathVariable long imageId) {
         ImageData imageData = imageService.getImageData(imageId);
         HttpHeaders headers = new HttpHeaders();
